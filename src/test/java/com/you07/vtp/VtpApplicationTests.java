520d750ea9dda6c608d2bffd5e05dec16f0b3f56
陈益岗 <1154610080@qq.com>
2019-11-22 15:36:16 +0800

    删除空间函数依赖

--- src/test/java/com/you07/vtp/VtpApplicationTests.java
+++ src/test/java/com/you07/vtp/VtpApplicationTests.java
@@ -1,23 +1,23 @@
-package com.you07.vtp;
-
-import com.you07.util.RestTemplateUtil;
-import com.you07.vtp.controller.LocationController;
-import com.you07.vtp.dao.LocationLatestDao;
-import com.you07.vtp.model.LocationLatest;
-import com.you07.vtp.service.LocationLatestService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import javax.annotation.Resource;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@WebAppConfiguration
-public class VtpApplicationTests {
-
-
-}
+//package com.you07.vtp;
+//
+//import com.you07.util.RestTemplateUtil;
+//import com.you07.vtp.controller.LocationController;
+//import com.you07.vtp.dao.LocationLatestDao;
+//import com.you07.vtp.model.LocationLatest;
+//import com.you07.vtp.service.LocationLatestService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//import javax.annotation.Resource;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@WebAppConfiguration
+//public class VtpApplicationTests {
+//
+//
+//}
